664ff66d066f8368d82f9326ba72913fa8963a8b
Trojan <ordrizzy@gmail.com>
2018-02-01 20:00:30 +0100

    wo ti ba penalty lo throwing

--- controllers/barcode-controller.js
+++ controllers/barcode-controller.js
@@ -1,7 +1,8 @@
 exports.create = (req, res, next) => {
   let text = req.body.text
+  let stamp = new Date()
 
   var qr_svg = qr.image(text, { type: 'svg' });
-  qr_svg.pipe(require('fs').createWriteStream('barcode.svg'));
+  qr_svg.pipe(require('fs').createWriteStream('barcode'+stamp'.svg'));
   var svg_string = qr.imageSync(text, { type: 'svg' });
 }
